326ff929b963d982f170cf549996085fabd7fffc
Rose <05rosa04@gmail.com>
2023-01-30 01:33:19 -0500

    Add edit form

---
+++ src/components/AlertBase.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { HStack, VStack, Text, Alert } from "native-base";
+
+const AlertBase = ({ text }) => {
+  return (
+    <Alert
+      maxWidth="100%"
+      alignSelf="center"
+      flexDirection="row"
+      status="success"
+      variant="left-accent"
+    >
+      <VStack space={2} flexShrink={1} w="80%">
+        <HStack space={2} flexShrink={1} alignItems="center">
+          <Alert.Icon />
+          <Text
+            fontSize="md"
+            fontWeight="medium"
+            flexShrink={1}
+            color="lightText"
+          >
+            {text}
+          </Text>
+        </HStack>
+      </VStack>
+    </Alert>
+  );
+};
+
+export default AlertBase;
--- src/pages/DetailAccount.jsx
+++ src/pages/DetailAccount.jsx
@@ -6,16 +6,85 @@ import {
   VStack,
   Text,
   Spacer,
-  CheckIcon,
   Icon,
   DeleteIcon,
+  Modal,
+  Button,
+  Input,
+  useToast,
 } from "native-base";
-import React from "react";
+import React, { useState } from "react";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { deleteDoc, doc, updateDoc } from "firebase/firestore";
+import { database } from "../config/fb";
+import AlertBase from "../components/AlertBase";
 
 const DetailAccount = ({ account = [] }) => {
+  const toast = useToast();
+  const [modalEdit, setModalEdit] = useState(false);
+  const [formEdit, setFormEdit] = useState({});
+
+  const onClickEdit = (infoEdit) => {
+    setModalEdit(!modalEdit);
+    setFormEdit(infoEdit);
+  };
+
+  const changeEdit = (text, name) => {
+    setFormEdit({
+      ...formEdit,
+      [name]: text,
+    });
+  };
+
+  const submitEdit = () => {
+    console.log("formEdit", formEdit);
+
+    const docRef = doc(database, "accounts", formEdit.id);
+    updateDoc(docRef, {
+      concept: formEdit.concept,
+    });
+
+    toast.show({
+      render: () => {
+        return <AlertBase text="Successful edition" />;
+      },
+    });
+
+    setModalEdit(false);
+  };
+
   return (
     <Box>
+      <Modal isOpen={modalEdit} onClose={() => setModalEdit(false)}>
+        <Modal.Content>
+          <Modal.CloseButton />
+          <Modal.Header>Edit</Modal.Header>
+          <Modal.Body>
+            <Input
+              variant="underlined"
+              w="100%"
+              value={formEdit.concept}
+              onChangeText={(e) => changeEdit(e, "concept")}
+              placeholder="Concept"
+            />
+          </Modal.Body>
+          <Modal.Footer>
+            <Button.Group space={2}>
+              <Button
+                variant="ghost"
+                colorScheme="blueGray"
+                onPress={() => {
+                  setModalEdit(false);
+                }}
+              >
+                Cancel
+              </Button>
+              <Button onPress={submitEdit}>Save</Button>
+            </Button.Group>
+          </Modal.Footer>
+        </Modal.Content>
+      </Modal>
+
       <FlatList
         p={5}
         data={account}
@@ -47,6 +116,9 @@ const DetailAccount = ({ account = [] }) => {
                     {item.concept}
                   </Text>
                   <Icon
+                    onPress={() => {
+                      onClickEdit(item);
+                    }}
                     as={MaterialCommunityIcons}
                     name="pencil-circle-outline"
                     color="blue.800"
@@ -74,7 +146,7 @@ const DetailAccount = ({ account = [] }) => {
                   _dark={{
                     color: "warmGray.50",
                   }}
-                  color={item.type === "1" ? "green.500" : "red.500"}
+                  color={item.type === "ingreso" ? "green.500" : "red.500"}
                   alignSelf="flex-start"
                 >
                   {item.value}
